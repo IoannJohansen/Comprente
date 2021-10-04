01689c9dba362691bac5bb9520900017bf019038
John Yo <boss.vanya20@mail.ru>
2021-10-04 22:23:58 +0300

    UI and API projects created

---
+++ ComprenteWebApi/src/main/java/com/comprent/ComprentApplication.java
@@ -0,0 +1,13 @@
+package com.comprent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComprentApplication.class, args);
+    }
+
+}
---
+++ ComprenteWebApi/src/test/java/com/comprent/ComprentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comprent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

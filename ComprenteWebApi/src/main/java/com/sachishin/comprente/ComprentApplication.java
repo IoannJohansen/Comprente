e8e103d562fd6ac9855798c8f699077b75d9368f
John Yo <boss.vanya20@mail.ru>
2021-10-05 00:47:58 +0300

    Layer atchitecture done + loger configured

--- ComprenteWebApi/src/main/java/com/sachishin/comprente/ComprentApplication.java
+++ ComprenteWebApi/src/main/java/com/sachishin/comprente/ComprentApplication.java
@@ -1,13 +1,11 @@
-package com.comprent;
+package com.sachishin.comprente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ComprentApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ComprentApplication.class, args);
     }
-
 }

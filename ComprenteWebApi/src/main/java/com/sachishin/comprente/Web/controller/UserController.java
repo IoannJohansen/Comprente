baf635850aa71b4ba7607212c61d243042149ba6
John Yo <boss.vanya20@mail.ru>
2021-12-17 19:33:43 +0300

    Added aspect for logging

--- ComprenteWebApi/src/main/java/com/sachishin/comprente/Web/controller/UserController.java
+++ ComprenteWebApi/src/main/java/com/sachishin/comprente/Web/controller/UserController.java
@@ -2,16 +2,11 @@ package com.sachishin.comprente.Web.controller;
 
 import com.sachishin.comprente.Repository.model.User;
 import com.sachishin.comprente.Service.UserService;
-import com.sachishin.comprente.validation.UserValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.DataBinder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -24,44 +19,12 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @RequestMapping(value="/user/")
-    public ResponseEntity<List<User>> listAllUser() {
-        List<User> users = userService.findAllUser();
-        if(users.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        log.warn("Processing request");
-        return new ResponseEntity<>(users, HttpStatus.OK);
+    @RequestMapping(value = "/login")
+    public String Login(String login, String password){
+
+        return "Hello wrodl!";
     }
 
-    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> getUser(@PathVariable("id") long id) {
-        User user = userService.findById(id);
-        if (user == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(user, HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "/user/", method = RequestMethod.POST)
-    public ResponseEntity<?> createUser(@RequestBody User user, UriComponentsBuilder ucBuilder) {
-        //Test validation
-        DataBinder binder = new DataBinder(user);
-        binder.setValidator(new UserValidator());
-        binder.validate();
-        BindingResult resVal = null;
-        resVal = binder.getBindingResult();
-        if(resVal.getAllErrors().size()!=0){
-            log.warn(resVal.toString());
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        //Test validation
-        if (userService.isUserExist(user)) {
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
-        }
-        userService.saveUser(user);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(ucBuilder.path("/api/user/{id}").buildAndExpand(user.getId()).toUri());
-        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
-    }
 }
+
+
---
+++ ComprenteWebApi/src/main/java/com/sachishin/comprente/aop/LogAspect.java
@@ -0,0 +1,34 @@
+package com.sachishin.comprente.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Aspect
+@Component
+@Slf4j
+public class LogAspect {
+    @Pointcut("execution(public * com.sachishin.comprente.Web.controller.UserController.*(..))")
+    public void callAtUserController() {
+    }
+
+    @Before("callAtUserController()")
+    public void beforeCallMethod(JoinPoint jp) {
+        String args = Arrays.stream(jp.getArgs())
+                .map(a -> a.toString())
+                .collect(Collectors.joining(","));
+        log.info("before " + jp.toString() + ", args=[" + args + "]");
+
+    }
+
+    @Before("callAtUserController()")
+    public void afterCallAt(JoinPoint jp) {
+        log.info("after " + jp.toString());
+    }
+}
